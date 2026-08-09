bcdd2fe2682815f108f523ed291ed7d11df7d27d
Your Name <you@company.com>
2026-08-09 18:08:04 +0330

    feat: establish application shell

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
-  selector: 'app-admin-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, NavigationComponent, RouterOutlet],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss',
 })
